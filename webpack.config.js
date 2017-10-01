3adc3a26fcf9b93ea223273e3e681435676bf4d5
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-09-30 21:29:25 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const webpack = require('webpack')
+ 
+module.exports = {
+    entry: './js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    devtool: 'source-map'
+}
