527c28123e5291e096481b533c18215b1f68054e
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-10-01 09:30:11 -0700

    Updates

--- example/scripts/SubComponent.js
+++ example/scripts/SubComponent.js
@@ -3,19 +3,23 @@ const PolymerComponent = require('@gkjohnson/react-polymer-component')
 const MyPolymerElement = PolymerComponent.bind('my-polymer-element')
 
 class SubComponent extends React.Component {
-
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return <div>
-            <div>HELLO! My Innards</div>
-            <my-polymer-element items={ this.props.props.items }>{this.props.props.text}</my-polymer-element>
+            <h1>Basic Polymer Element</h1>
+            <my-polymer-element
+                items={ this.props.props.items }
+                on-click={ e => console.log('Polymer Click Event') }
+                on-my-event={ e => console.log('Customy Polymer Event') }
+            >
+                {this.props.props.text}
+                <div>THING</div>
+            </my-polymer-element>
+            
+            <h1>Wrapped Polymer Element</h1>
             <MyPolymerElement
                 items={ this.props.props.items }
-                on-click={ e => console.log("CLICKED") }
-                on-my-event={ e => console.log("MY EVENT") }
+                on-click={ e => console.log('Polymer Click Event') }
+                on-my-event={ e => console.log('Customy Polymer Event') }
             >
                 {this.props.props.text}
                 <div>THING</div>
